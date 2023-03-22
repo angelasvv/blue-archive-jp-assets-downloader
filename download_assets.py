d280f7206f403d3ecf7933c95fd55c1ca1b13709
Nemo Xiong <xiongnemo@126.com>
2023-03-22 16:35:40 +0800

    送我春月

--- download_assets.py
+++ download_assets.py
@@ -6,7 +6,7 @@ import os
 # set current version from environ
 current_version = os.environ.get('BA_JP_CURRENT_VERSION', None)
 if not current_version:
-    current_version = 'r53_29_rfhqxfw36obfz83ei505'
+    current_version = 'r54_40vw6q1p44ki189rr0vr'
 
 # set up logging
 logging.basicConfig(level=logging.INFO,
