11eb174fe7a9e8f0091ceb0557ed13433b4d56a1
Z <angelasvv@126.com>
2025-07-23 16:46:22 +0800

    Update download_assets.py

--- download_assets.py
+++ download_assets.py
@@ -6,7 +6,7 @@ import os
 # set current version from environ
 current_version = os.environ.get('BA_JP_CURRENT_VERSION', None)
 if not current_version:
-    current_version = 'r81_58_ocv432iagl3hsufr8w1d'
+    current_version = 'r82_59_hhfkpxf94r4f9sgct1y9'
 
 # set up logging
 logging.basicConfig(level=logging.INFO,
